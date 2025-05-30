90900aba38db5c9409db7a6326c84d79b2663649
Miro Palmu <email@miropalmu.cc>
2025-05-30 16:59:41 +0300

    feat(mdgrid_buffer): implement initial mdgrid_buffer::mds

--- src/tyvi/mdgrid_buffer.h
+++ src/tyvi/mdgrid_buffer.h
@@ -10,12 +10,109 @@ namespace tyvi {
 
 template<typename V, typename ElemExtents, typename ElemLP, typename GridExtents, typename GridLP>
 class [[nodiscard]] mdgrid_buffer {
-    using grid_mapping_type = GridLP::template mapping<GridExtents>;
+    /// Element type of grid is always mdspan, so element element type is the innermost element type.
+    using element_element_type = V::value_type;
+    using element_mapping_type = ElemLP::template mapping<ElemExtents>;
+    using grid_mapping_type    = GridLP::template mapping<GridExtents>;
 
+    static_assert(ElemExtents::rank_dynamic() == 0);
+    static_assert(element_mapping_type::is_always_exhaustive());
+
+    static constexpr element_mapping_type element_mapping_{};
     grid_mapping_type grid_mapping_;
 
+    V buff_;
+
+    /* Constness of mdgrid_buffer can not be deduced outside of member functions.
+       So the accessor policies have to be templated. */
+
+    template<bool has_const>
+    struct [[nodiscard]]
+    element_accessor_policy {
+        std::size_t grid_offset;
+        std::size_t grid_required_span_size;
+
+        using element_type =
+            std::conditional_t<has_const, const element_element_type, element_element_type>;
+        using data_handle_type =
+            std::conditional_t<has_const, typename V::const_pointer, typename V::pointer>;
+        using reference =
+            std::conditional_t<has_const, typename V::const_reference, typename V::reference>;
+        using offset_policy = element_accessor_policy;
+
+        constexpr reference access(this const auto& self,
+                                   data_handle_type const buff_ptr,
+                                   const std::size_t element_offset) {
+            return buff_ptr[(element_offset * self.grid_required_span_size) + self.grid_offset];
+        }
+
+        constexpr offset_policy::data_handle_type offset(this const auto& self,
+                                                         data_handle_type const buff_ptr,
+                                                         const std::size_t element_offset) {
+            return buff_ptr[element_offset * self.grid_required_span_size];
+        }
+    };
+
+    template<bool has_const>
+    using element_mdspan = std::mdspan<typename element_accessor_policy<has_const>::element_type,
+                                       ElemExtents,
+                                       ElemLP,
+                                       element_accessor_policy<has_const>>;
+
+    template<bool has_const>
+    struct [[nodiscard]]
+    grid_accessor_policy {
+        std::size_t grid_required_span_size;
+
+        using element_type = element_mdspan<has_const>;
+        struct data_handle_type {
+            using buff_ptr_type =
+                std::conditional_t<has_const, typename V::const_pointer, typename V::pointer>;
+
+            buff_ptr_type buff_ptr;
+            std::size_t grid_offset{ 0uz };
+        };
+        using reference     = element_type;
+        using offset_policy = grid_accessor_policy;
+
+        constexpr reference access(this const auto& self,
+                                   data_handle_type const grid_handle,
+                                   const std::size_t grid_offset) {
+            const auto acc = element_accessor_policy<has_const>{
+                .grid_offset             = grid_offset + grid_handle.grid_offset,
+                .grid_required_span_size = self.grid_required_span_size
+            };
+            return element_mdspan<has_const>(grid_handle.buff_ptr, element_mapping_, acc);
+        }
+
+        constexpr offset_policy::data_handle_type offset(this const auto&,
+                                                         data_handle_type const grid_handle,
+                                                         const std::size_t grid_offset) {
+            auto new_handle = grid_handle;
+            new_handle.grid_offset += grid_offset;
+            return new_handle;
+        }
+    };
+
   public:
-    explicit constexpr mdgrid_buffer(const grid_mapping_type& m) : grid_mapping_(m) {}
+    explicit constexpr mdgrid_buffer(const grid_mapping_type& m)
+        : grid_mapping_(m),
+          buff_(element_mapping_.required_span_size() * grid_mapping_.required_span_size()) {}
+
+    template<typename Self>
+    [[nodiscard]]
+    constexpr auto mds(this Self& self) {
+        static constexpr bool has_const = std::is_const_v<std::remove_reference_t<Self>>;
+
+        using grid_acc = grid_accessor_policy<has_const>;
+        using grid_mdspan =
+            std::mdspan<typename grid_acc::element_type, GridExtents, GridLP, grid_acc>;
+
+        return grid_mdspan(
+            typename grid_acc::data_handle_type{ .buff_ptr{ self.buff_.data() } },
+            self.grid_mapping_,
+            grid_acc{ .grid_required_span_size = self.grid_mapping_.required_span_size() });
+    }
 };
 
 } // namespace tyvi
--- test/test_mdgrid_buffer.c++
+++ test/test_mdgrid_buffer.c++
@@ -1,6 +1,8 @@
 #include <boost/ut.hpp> // import boost.ut;
 
 #include <cstddef>
+#include <ranges>
+#include <type_traits>
 #include <vector>
 
 #include <experimental/mdspan>
@@ -33,6 +35,46 @@ const suite<"mdgrid_buffer"> _ = [] {
         [[maybe_unused]]
         auto _ = testing_mdgrid_buffer(grid_mapping);
         expect(true);
+    };
+
+    "mdgrid_buffer is copyable and const correct viewable through mdspan"_test = [] {
+        const auto grid_mapping =
+            grid_layout_policy::template mapping<grid_extents>(grid_extents{ 2, 3, 4 });
+
+        [[maybe_unused]]
+        auto buff = testing_mdgrid_buffer(grid_mapping);
+
+        const auto mds = buff.mds();
+
+        expect(not std::is_const_v<
+               std::remove_reference_t<typename decltype(mds)::element_type::reference>>);
+
+        namespace rv = std::views;
+        auto i_space = rv::iota(0uz, 2uz);
+        auto j_space = rv::iota(0uz, 3uz);
+        auto k_space = rv::iota(0uz, 4uz);
+
+        auto bad_hash = [](const auto i, const auto j, const auto k, const auto idx) {
+            return static_cast<element_type>(i + 10 * j + 100 * k + 1000 * idx[0] + 10000 * idx[1]);
+        };
+
+        for (const auto [i, j, k] : rv::cartesian_product(i_space, j_space, k_space)) {
+            for (const auto idx : tyvi::sstd::geometric_index_space<2, 2>()) {
+                mds[i, j, k][idx] = bad_hash(i, j, k, idx);
+            }
+        }
+
+        const auto cbuff = buff;
+        const auto cmds  = cbuff.mds();
+
+        expect(std::is_const_v<
+               std::remove_reference_t<typename decltype(cmds)::element_type::reference>>);
+
+        for (const auto [i, j, k] : rv::cartesian_product(i_space, j_space, k_space)) {
+            for (const auto idx : tyvi::sstd::geometric_index_space<2, 2>()) {
+                expect(cmds[i, j, k][idx] == bad_hash(i, j, k, idx));
+            }
+        }
     };
 };
 
