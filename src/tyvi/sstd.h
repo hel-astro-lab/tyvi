84c472262f56c36456d7a866bf36c21de8a96d35
Miro Palmu <email@miropalmu.cc>
2025-11-09 16:14:44 +0200

    feat(sstd): implement immovable struct

--- src/tyvi/sstd.h
+++ src/tyvi/sstd.h
@@ -20,4 +20,15 @@ ipow(const T base, const T exponent) {
     return result;
 }
 
+struct immovable {
+    constexpr immovable()           = default;
+    constexpr ~immovable() noexcept = default;
+
+    constexpr immovable(immovable&&) noexcept                  = delete; // move constructor
+    constexpr immovable& operator=(immovable&& other) noexcept = delete; // move assignment
+
+    constexpr immovable(const immovable&)            = delete; // copy constructor
+    constexpr immovable& operator=(const immovable&) = delete; // copy assignment
+};
+
 } // namespace tyvi::sstd
