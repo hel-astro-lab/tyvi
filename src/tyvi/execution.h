0fab1855b4a4947828c1826bbf1aee5af819521e
Miro Palmu <email@miropalmu.cc>
2025-12-19 23:29:42 +0200

    deps: expose std::execution from pika through tyvi/execution.h

---
+++ src/tyvi/execution.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <pika/execution.hpp>
+
+namespace tyvi {
+namespace exec        = pika::execution::experimental;
+namespace this_thread = pika::this_thread::experimental;
+} // namespace tyvi
