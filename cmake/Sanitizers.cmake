ca96caeab8759d9b89297fadb83ff3ea580f45b2
Miro Palmu <email@miropalmu.cc>
2026-05-14 16:01:32 +0300

    license: use correct copyright statement

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,4 +1,4 @@
-# Copyright 2018 - 2026, Miro Palmu and the hel-astro-lab contributors
+# Copyright 2025 - 2026, Miro Palmu and the tyvi contributors
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Setup sanitizers from arguments for target.
--- src/tyvi/backend.h
+++ src/tyvi/backend.h
@@ -1,4 +1,4 @@
-// Copyright 2018 - 2026, Miro Palmu and the hel-astro-lab contributors
+// Copyright 2026 - 2026, Miro Palmu and the tyvi contributors
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #pragma once
