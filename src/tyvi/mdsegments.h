995624240093489fbc439e2a31039fc93bbc87d4
Miro Palmu <email@miropalmu.cc>
2026-06-06 21:14:11 +0300

    feat: implement mdsegments

---
+++ src/tyvi/mdsegments.h
@@ -0,0 +1,350 @@
+// Copyright 2025 - 2026, Miro Palmu and the tyvi contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <tuple>
+#include <vector>
+
+#include "tyvi/mdspan.h"
+
+namespace tyvi {
+
+template<typename T,
+         std::size_t SegmentSize,
+         typename E,
+         typename LP        = std::layout_right,
+         typename Allocator = std::allocator<T>>
+class [[nodiscard]]
+mdsegments {
+    static_assert(E::rank_dynamic() == 0);
+
+  public:
+    using allocator_type   = Allocator;
+    using allocator_traits = std::allocator_traits<Allocator>;
+
+    // TODO: support allocator aware constructors (there is no need atm).
+
+    explicit constexpr mdsegments() = default;
+    explicit constexpr mdsegments(std::size_t);
+
+    constexpr ~mdsegments()                            = default;
+    constexpr mdsegments(const mdsegments&)            = default;
+    constexpr mdsegments(mdsegments&&)                 = default;
+    constexpr mdsegments& operator=(const mdsegments&) = default;
+    constexpr mdsegments& operator=(mdsegments&&)      = default;
+
+    [[nodiscard]]
+    constexpr bool empty() const;
+    [[nodiscard]]
+    constexpr std::size_t size() const;
+
+    constexpr void resize(std::size_t);
+
+    template<typename U>
+    struct [[nodiscard]] inner_accessor_policy;
+
+    template<typename U>
+    struct [[nodiscard]] outer_accessor_policy;
+
+    template<typename U>
+    using inner_mds = std::mdspan<U, E, LP, inner_accessor_policy<U>>;
+
+    template<typename U>
+    using outer_mds = std::mdspan<inner_mds<U>,
+                                  std::extents<std::size_t, std::dynamic_extent>,
+                                  std::layout_right,
+                                  outer_accessor_policy<U>>;
+
+    [[nodiscard]]
+    constexpr outer_mds<T> mds();
+
+    [[nodiscard]]
+    constexpr outer_mds<const T> mds() const;
+
+    template<typename U>
+    struct [[nodiscard]] raw_view_type : std::ranges::view_interface<raw_view_type<U>> {
+        using iterator_category = std::random_access_iterator_tag;
+        using value_type        = U;
+        using size_type         = std::size_t;
+        using difference_type   = std::ptrdiff_t;
+        using reference         = value_type&;
+
+        class [[nodiscard]] iterator_type;
+        using iterator = iterator_type;
+
+        constexpr iterator begin() const;
+        constexpr iterator end() const;
+
+        iterator begin_;
+        iterator end_;
+    };
+
+    constexpr raw_view_type<T> raw_view();
+    constexpr raw_view_type<const T> raw_view() const;
+
+  private:
+    using allocator_value_type = typename allocator_traits::value_type;
+    using allocator_pointer    = typename allocator_traits::pointer;
+
+    using segment_ptr_allocator = allocator_traits::template rebind_alloc<allocator_pointer>;
+    using segment_ptr_allocator_traits =
+        allocator_traits::template rebind_traits<allocator_pointer>;
+
+    std::vector<allocator_pointer> segments_{};
+    segment_ptr_allocator_traits::pointer segment_ptrs_{ nullptr };
+    std::size_t outer_size_{};
+
+    [[no_unique_address]]
+    Allocator allocator_{};
+    [[no_unique_address]]
+    segment_ptr_allocator segment_ptr_allocator_{ allocator_ };
+};
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+template<typename U>
+struct mdsegments<T, SG, E, LP, A>::inner_accessor_policy {
+    using element_type     = U;
+    using data_handle_type = mdsegments::raw_view_type<U>::iterator;
+    using reference        = U&;
+    using offset_policy    = mdsegments::inner_accessor_policy<U>;
+
+    [[nodiscard]]
+    constexpr reference access(data_handle_type const h, const std::size_t offset) const {
+        return *std::ranges::next(h, static_cast<std::ptrdiff_t>(offset * SG));
+    }
+
+    [[nodiscard]]
+    constexpr offset_policy::data_handle_type offsest(data_handle_type const h,
+                                                      const std::size_t offset) const {
+        return std::ranges::next(h, static_cast<std::ptrdiff_t>(offset * SG));
+    }
+};
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+template<typename U>
+struct mdsegments<T, SG, E, LP, A>::outer_accessor_policy {
+    using element_type = mdsegments::inner_mds<U>;
+    using data_handle_type =
+        std::tuple<typename mdsegments::raw_view_type<U>::iterator, std::size_t>;
+    using reference     = element_type;
+    using offset_policy = mdsegments::outer_accessor_policy<U>;
+
+    [[nodiscard]]
+    constexpr reference access(data_handle_type const h, const std::size_t offset) const {
+        const auto b         = std::get<0>(h);
+        const auto n         = offset + std::get<1>(h);
+        const auto segments  = n / SG;
+        const auto left_over = n % SG;
+        const auto m         = typename LP::template mapping<E>{};
+        const auto rss       = m.required_span_size();
+        const auto skip      = segments * SG * rss + left_over;
+        return element_type(std::ranges::next(b, static_cast<std::ptrdiff_t>(skip)));
+    }
+
+    [[nodiscard]]
+    constexpr offset_policy::data_handle_type offsest(data_handle_type const h,
+                                                      const std::size_t offset) const {
+        return { std::get<0>(h), std::get<1>(h) + offset };
+    }
+};
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+constexpr auto
+mdsegments<T, SG, E, LP, A>::mds() -> outer_mds<T> {
+    return mdsegments::outer_mds<T>({ this->raw_view().begin(), 0uz }, this->size());
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+constexpr auto
+mdsegments<T, SG, E, LP, A>::mds() const -> outer_mds<const T> {
+    return mdsegments::outer_mds<const T>({ this->raw_view().begin(), 0uz }, this->size());
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+constexpr bool
+mdsegments<T, SG, E, LP, A>::empty() const {
+    return this->size() == 0;
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+constexpr std::size_t
+mdsegments<T, SG, E, LP, A>::size() const {
+    return this->outer_size_;
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+constexpr void
+mdsegments<T, SG, E, LP, A>::resize(const std::size_t outer_size) {
+    const auto m                  = typename LP::template mapping<E>{};
+    const auto rss                = m.required_span_size();
+    const auto required_segments  = outer_size ? (outer_size - 1uz) / SG + 1uz : 0uz;
+    const auto need_more_segments = required_segments > this->segments_.size();
+    if (need_more_segments) {
+        const auto how_many_more = required_segments - this->segments_.size();
+        for (auto _ : std::views::iota(0uz, how_many_more)) {
+            this->segments_.push_back(allocator_traits::allocate(this->allocator_, SG * rss));
+        }
+    }
+    this->outer_size_ = outer_size;
+
+    this->segment_ptrs_ = segment_ptr_allocator_traits::allocate(this->segment_ptr_allocator_,
+                                                                 this->segments_.size());
+
+    for (auto i = 0uz; i < this->segments_.size(); ++i) {
+        segment_ptr_allocator_traits::construct(
+            this->segment_ptr_allocator_,
+            std::ranges::next(this->segment_ptrs_, static_cast<std::ptrdiff_t>(i)),
+            this->segments_[i]);
+    }
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+template<typename U>
+constexpr auto
+mdsegments<T, SG, E, LP, A>::raw_view_type<U>::begin() const -> iterator {
+    return this->begin_;
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+template<typename U>
+constexpr auto
+mdsegments<T, SG, E, LP, A>::raw_view_type<U>::end() const -> iterator {
+    return this->end_;
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+constexpr mdsegments<T, SG, E, LP, A>::mdsegments(const std::size_t n) : mdsegments() {
+    this->resize(n);
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+template<typename U>
+class mdsegments<T, SG, E, LP, A>::raw_view_type<U>::iterator_type {
+  public:
+    using iterator_category = std::random_access_iterator_tag;
+    using difference_type   = mdsegments<T, SG, E, LP, A>::raw_view_type<U>::difference_type;
+    using value_type        = mdsegments::allocator_value_type;
+    using reference         = mdsegments::allocator_value_type&;
+    using pointer           = mdsegments::allocator_pointer;
+
+    constexpr iterator_type() = default;
+    constexpr iterator_type(mdsegments::segment_ptr_allocator_traits::pointer const ptr,
+                            const std::size_t n)
+        : ptr_{ ptr },
+          offset_{ n } {}
+
+    /// prefix increment
+    constexpr iterator_type& operator++() {
+        ++offset_;
+        return *this;
+    }
+
+    /// postfix increment
+    [[nodiscard]]
+    constexpr iterator_type operator++(int) {
+        iterator_type old = *this;
+        ++(*this);
+        return old;
+    }
+
+    /// prefix decrement
+    constexpr iterator_type& operator--() {
+        --offset_;
+        return *this;
+    }
+
+    /// postfix decrement
+    [[nodiscard]]
+    constexpr iterator_type operator--(int) {
+        iterator_type old = *this;
+        --(*this);
+        return old;
+    }
+
+    [[nodiscard]]
+    constexpr reference operator*() const {
+        const auto m         = typename LP::template mapping<E>{};
+        const auto rss       = m.required_span_size();
+        const auto segment   = this->offset_ / (rss * SG);
+        const auto left_over = this->offset_ % (rss * SG);
+
+        return this->ptr_[segment][left_over];
+    }
+
+    [[nodiscard]]
+    constexpr auto operator<=>(const iterator_type& rhs) const {
+        return this->offset_ <=> rhs.offset_;
+    }
+
+    [[nodiscard]]
+    constexpr bool operator==(const iterator_type& rhs) const {
+        return (this->ptr_ == rhs.ptr_) and (this->offset_ == rhs.offset_);
+    }
+
+    [[nodiscard]]
+    friend constexpr difference_type operator-(const iterator_type& lhs, const iterator_type& rhs) {
+        if (lhs.offset_ >= rhs.offset_) {
+            return static_cast<difference_type>(lhs.offset_ - rhs.offset_);
+        }
+        return -static_cast<difference_type>(rhs.offset_ - lhs.offset_);
+    }
+
+    [[nodiscard]]
+    friend constexpr iterator_type operator+(const iterator_type& lhs, const difference_type rhs) {
+        auto result    = lhs;
+        result.offset_ = static_cast<std::size_t>(static_cast<difference_type>(lhs.offset_) + rhs);
+        return result;
+    }
+
+    [[nodiscard]]
+    friend constexpr iterator_type operator+(const difference_type lhs, const iterator_type& rhs) {
+        return rhs + lhs;
+    }
+
+    [[nodiscard]]
+    friend constexpr iterator_type operator-(const iterator_type& lhs, const difference_type rhs) {
+        return lhs + (-rhs);
+    }
+
+    constexpr iterator_type& operator+=(const difference_type rhs) { return *this = *this + rhs; }
+
+    constexpr iterator_type& operator-=(const difference_type rhs) { return *this = *this - rhs; }
+
+    [[nodiscard]]
+    constexpr reference operator[](const difference_type rhs) const {
+        return *(*this + rhs);
+    }
+
+  private:
+    mdsegments::segment_ptr_allocator_traits::pointer ptr_{ nullptr };
+    std::size_t offset_{ 0 };
+};
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+constexpr auto
+mdsegments<T, SG, E, LP, A>::raw_view() -> raw_view_type<T> {
+    const auto m   = typename LP::template mapping<E>{};
+    const auto rss = m.required_span_size();
+    return raw_view_type<T>{
+        .begin_ = typename raw_view_type<T>::iterator(this->segment_ptrs_, 0uz),
+        .end_   = typename raw_view_type<T>::iterator(this->segment_ptrs_,
+                                                    SG * rss * this->segments_.size())
+    };
+}
+
+template<typename T, std::size_t SG, typename E, typename LP, typename A>
+constexpr auto
+mdsegments<T, SG, E, LP, A>::raw_view() const -> raw_view_type<const T> {
+    const auto m   = typename LP::template mapping<E>{};
+    const auto rss = m.required_span_size();
+    return raw_view_type<const T>{
+        .begin_ = typename raw_view_type<const T>::iterator(this->segment_ptrs_, 0uz),
+        .end_   = typename raw_view_type<const T>::iterator(this->segment_ptrs_,
+                                                          SG * rss * this->segments_.size())
+    };
+}
+} // namespace tyvi
---
+++ test/test_mdsegments.c++
@@ -0,0 +1,184 @@
+// Copyright 2025 - 2026, Miro Palmu and the tyvi contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <boost/ut.hpp> // import boost.ut;
+
+#include <algorithm>
+#include <cstddef>
+#include <iterator>
+#include <memory>
+#include <print>
+#include <ranges>
+#include <type_traits>
+#include <vector>
+
+#include "tyvi/mdsegments.h"
+#include "tyvi/mdspan.h"
+#include "tyvi/sstd.h"
+
+namespace {
+using namespace boost::ut;
+
+[[maybe_unused]]
+const suite<"mdsegments"> _ = [] {
+    using T                 = int;
+    static constexpr auto N = 3;
+    using E                 = tyvi::sstd::geometric_extents<2, 2>;
+    using LP                = std::layout_right;
+    using allocator         = std::allocator<T>;
+    using segments          = tyvi::mdsegments<T, N, E, LP, allocator>;
+
+    "default constructed is empty"_test = [] {
+        auto s = segments();
+        expect(s.empty());
+    };
+
+    "size can be changed"_test = [] {
+        auto s = segments(10);
+        expect(not s.empty());
+        expect(s.size() == 10uz);
+        s.resize(17);
+        expect(s.size() == 17uz);
+    };
+
+    "raw_view"_test = [] {
+        auto s = segments(17);
+
+        expect(s.raw_view().size() == 18 * LP::mapping<E>{}.required_span_size());
+
+        for (auto& x : s.raw_view()) { x = 42; }
+        for (const auto& x : static_cast<const segments&>(s).raw_view()) { expect(x == 42); }
+
+        expect(std::ranges::view<decltype(s.raw_view())>);
+    };
+
+    "raw_view is random access range"_test = [] {
+        auto s       = segments(6); // such that raw view is 24 elements.
+        const auto v = s.raw_view();
+        expect(v.size() == 24);
+
+        using iterator_type = std::ranges::iterator_t<decltype(v)>;
+        using sentinel_type = std::ranges::sentinel_t<decltype(v)>;
+        using range_type    = decltype(v);
+
+        expect(std::random_access_iterator<iterator_type>);
+        expect(std::sentinel_for<sentinel_type, iterator_type>);
+        expect(std::ranges::random_access_range<range_type>);
+
+        std::ranges::iota(v, 0);
+
+        auto b = std::ranges::begin(v);
+        auto e = std::ranges::end(v);
+
+        auto test = [](const auto x, const auto e) {
+            expect(x == e) << std::format("{} != {}", x, e);
+        };
+
+        test(*b++, 0);
+
+        --b;
+
+        test(*++b, 1);
+
+        using difference_type = std::iter_difference_t<iterator_type>;
+
+        const auto n10 = difference_type{ 10 };
+        const auto n4  = difference_type{ 4 };
+
+        b = b + n10;
+
+        test(*b, 11);
+
+        b += n4;
+
+        test(*b, 15);
+
+        b = n4 + b;
+
+        test(*b, 19);
+
+        const auto m = e - b;
+
+        test(m, difference_type{ 5 });
+
+        b -= m;
+
+        test(*b--, 14);
+        test(*--b, 12);
+
+        b = b - m;
+
+        test(*b, 7);
+
+        test(b[n10], 17);
+
+        expect(b < e);
+        expect(b <= e);
+        expect(not(b > e));
+        expect(not(b >= e));
+        expect(b == b); // NOLINT
+        expect(e == e); // NOLINT
+        expect(b != e);
+
+        expect(b + difference_type{ 17 } == e);
+    };
+
+    "resize does not invalidate references"_test = [] {
+        auto s                 = segments(10);
+        const auto to_pointers = std::views::transform([](const auto& x) { return &x; });
+
+        const auto a = s.raw_view() | to_pointers | std::ranges::to<std::vector>();
+        s.resize(10000);
+        const auto b = s.raw_view() | to_pointers | std::views::take(std::ranges::size(a))
+                       | std::ranges::to<std::vector>();
+        expect(a == b);
+    };
+
+    "writeable through mdspan"_test = [] {
+        auto s         = segments(15);
+        const auto mds = s.mds();
+
+        for (const auto idx : tyvi::sstd::index_space(mds)) {
+            for (const auto tidx : tyvi::sstd::index_space(mds[idx])) { mds[idx][tidx] = 42; }
+        }
+
+        expect(std::ranges::all_of(s.raw_view(), [](const auto x) { return x == 42; }));
+    };
+
+    "const correct viewable through mdspan"_test = [] {
+        auto s         = segments(7);
+        const auto mds = s.mds();
+
+        expect(not std::is_const_v<
+               std::remove_reference_t<typename decltype(mds)::element_type::reference>>);
+
+        auto bad_hash = [](const auto n, const auto i, const auto j) {
+            return static_cast<int>(4 * n + 2 * i + j);
+        };
+
+        for (const auto idx : tyvi::sstd::index_space(mds)) {
+            for (const auto tidx : tyvi::sstd::index_space(mds[idx])) {
+                mds[idx][tidx] = bad_hash(idx[0], tidx[0], tidx[1]);
+            }
+        }
+
+        const auto& cs  = static_cast<const segments&>(s);
+        const auto cmds = cs.mds();
+
+        expect(std::is_const_v<
+               std::remove_reference_t<typename decltype(cmds)::element_type::reference>>);
+
+        for (const auto idx : tyvi::sstd::index_space(cmds)) {
+            for (const auto tidx : tyvi::sstd::index_space(cmds[idx])) {
+                expect(cmds[idx][tidx] == bad_hash(idx[0], tidx[0], tidx[1]));
+            }
+        }
+    };
+};
+
+} // namespace
+
+int
+main(int argc, const char** argv) {
+    return static_cast<int>(cfg<override>.run(run_cfg{ .argc = argc, .argv = argv }));
+}
