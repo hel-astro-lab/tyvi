84e4c921a0300ac4538cde74e9022dfc03441e4d
Juhana Lankinen <juhana.lankinen@csc.fi>
2026-09-18 11:20:48 +0300

    Verify taylor expansion

--- bench-test/bench.cpp
+++ bench-test/bench.cpp
@@ -1,26 +1,55 @@
 #include "tyvi/mdgrid.h"
 #include "tyvi/mdspan.h"
 #include <array>
+#include <cassert>
 #include <chrono>
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
+#include <iostream>
 #include <thrust/sequence.h>
+
+using T = float;
+
+auto normalize(auto x, auto n) {
+    return T{ 1.0 } + (5.0 - 1.0) * static_cast<T>(x) / static_cast<T>(n);
+}
+
+void
+verify(const auto& vec) {
+    const T limit    = 0.01f;
+    bool approx_same = true;
+    for (auto i = 0uz; i < vec.size(); i++) {
+        const auto ref = std::exp(normalize(i, vec.size()));
+        const bool less =
+            limit > std::abs(ref - vec[i]);
+        approx_same &= less;
+        if (not less) {
+            std::printf("Not less than limit for %d: %f, (ref = %f) (limit = %f)\n",
+                        i,
+                        vec[i],
+                        ref,
+                        limit);
+        }
+    }
+    assert(approx_same);
+}
 
 template <std::size_t N>
 double work() {
-    using T = float;
-    auto vec = thrust::device_vector<T>(10000000);
+    static constexpr std::size_t M = 10000000;
+    auto vec = thrust::device_vector<T>(M);
     const auto w = tyvi::mdgrid_work{};
     thrust::sequence(w.on_this(), vec.begin(), vec.end());
 
-    // begin measure
     const auto start = std::chrono::steady_clock::now();
     thrust::for_each(w.on_this(), vec.begin(), vec.end(), [](auto& x) {
         using T = std::remove_cvref_t<decltype(x)>;
         T sum = T{1.0};
         T term = T{1.0};
+        const T y = normalize(x, M);
         for (std::size_t n = 1; n <= N; n++) {
-            term *= x / n;
+            term *= y / n;
             sum += term;
         }
         x = sum;
@@ -29,7 +58,10 @@ double work() {
     const auto end = std::chrono::steady_clock::now();
     const std::chrono::duration<double> diff = end - start;
 
-    const auto host_vec = thrust::host_vector<T>(vec.begin(), vec.end());
+    if constexpr (N == 16) {
+        const auto host_vec = thrust::host_vector<T>(vec.begin(), vec.end());
+        verify(host_vec);
+    }
 
     return diff.count();
 }
